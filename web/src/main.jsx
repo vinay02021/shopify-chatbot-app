ecd856e91e18dc813762fe8e5a6db0a400723d47
Vinay Singh <technology.vinay26@gmail.com>
2026-05-02 18:17:20 +0530

    updated code

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,6 +3,21 @@ import { createRoot } from "react-dom/client";
 import "@shopify/polaris/build/esm/styles.css";
 import App from "./App.jsx";
 
+import createApp from "@shopify/app-bridge";
+
+const params = new URLSearchParams(window.location.search);
+
+const host = params.get("host");
+
+const app = createApp({
+  apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
+  host: host,
+  forceRedirect: true,
+});
+
+// debug
+window.app = app;
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
