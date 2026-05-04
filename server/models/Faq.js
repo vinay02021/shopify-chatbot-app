2fe6daf12f39e0490336d9329a245d6d2f52c08e
Vinay Singh <technology.vinay26@gmail.com>
2026-05-04 12:18:13 +0530

    add shop field

--- server/models/Faq.js
+++ server/models/Faq.js
@@ -4,8 +4,15 @@ const faqSchema = new mongoose.Schema(
   {
     question: String,
     answer: String,
+
+    // 🔥 ADD THIS
+    shop: {
+      type: String,
+      required: true,
+      index: true,
+    },
   },
-  { timestamps: true } 
+  { timestamps: true }
 );
 
 export const Faq = mongoose.model("Faq", faqSchema);
